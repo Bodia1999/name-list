1de35ee59bf87a97dfe51bd1320b9888a6520ce7
Bodia1999 <92782560bodia>
2019-06-03 23:16:50 +0300

    new

--- name-list/src/app/pipes/letter-to-upper-case.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LetterToUpperCasePipe } from './letter-to-upper-case.pipe';
-
-describe('LetterToUpperCasePipe', () => {
-  it('create an instance', () => {
-    const pipe = new LetterToUpperCasePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
